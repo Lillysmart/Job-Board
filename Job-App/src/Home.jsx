d9c5cd8977bb8e5e0fde747572fc1c4e2cfbac72
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-01-12 16:06:44 +0200

    edit the buttoms

--- Job-App/src/Home.jsx
+++ Job-App/src/Home.jsx
@@ -1,16 +1,44 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-export const HomePage= ()=>{
+export const HomePage = () => {
     return (
-        <div>
-            <button className='search-button' type='search'>Search..</button>
-            <h1>Find Your Perfect Job ... </h1>
+      <div>
+        <div className='header'>
+          <input className='top-button' type='search' placeholder='Job Title / Keyword...' />
           
-            
-           
-        </div>)
-}
+         
+          <label htmlFor="location">Location :</label>
+
+          <select id="location" name="location">
+            <option value="All">Wordwide</option>
+            <option value="Johannesburg">Johannesburg</option>
+            <option value="Cape Town">Cape Town</option>
+            <option value="Bloemfontein">Bloemfontein</option>
+            <option value="Pretoria">Pretoria</option>
+            <option value="Sandton">Sandton</option>
+            <option value="Durban">Durban</option>
+            <option value="East London">East London</option>
+          </select>
+          
+
+          <button className='search-button'>
+        Search
+        </button>
+
+
+        </div>
+        <h1>Find Your Perfect Job ...</h1>
+
+
+        <div>
+
+
+      
+        </div>
+      </div>
+    );
+  };
 
 export default HomePage
 
