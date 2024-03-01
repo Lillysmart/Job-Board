994e2f4ecead28fda820793194957576cbdd827b
Lillysmart <137174363+Lillysmart@users.noreply.github.com>
2024-03-01 10:27:04 +0200

    fetch the data from the API

--- Job-App/src/Home.jsx
+++ Job-App/src/Home.jsx
@@ -1,53 +1,69 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { useEffect, useState } from "react";
 
 export const HomePage = () => {
-    return (
-      <div>
-        <div className='header'>
-            <img className='header-img' src= '/logo-no-background.png' width={200} height={40} />
-          <input className='top-button' type='search' placeholder='Job Title / Keyword...' />
-          
-         
-          <label htmlFor="location"></label>
+  const { data, setData } = useState([]);
 
-          <select id="location" name="location">
-            <option value="All">Wordwide</option>
-            <option value="Johannesburg">Johannesburg</option>
-            <option value="Cape Town">Cape Town</option>
-            <option value="Bloemfontein">Bloemfontein</option>
-            <option value="Pretoria">Pretoria</option>
-            <option value="Sandton">Sandton</option>
-            <option value="Durban">Durban</option>
-            <option value="East London">East London</option>
-          </select>
-          
-
-          <button className='search-button'>
-        Search
-        </button>
-
-<img src='/login.png' className='header-image'width={60} height={50} />
-        </div>
-
-        <div className="job-section">
-  <div className="job-text">
-    <h2>Find Your Perfect...</h2>
-   
-    
-  </div>
-  <img src='/search.png' alt='Search Image' className="job-image" />
-</div>
-
-        <div>
-
-
+  useEffect(() => {
+    fetch("https://www.arbeitnow.com/api/job-board-api") // api for the get request
+      .then((response) => response.json())
+      .then((data) => console.log(data));
       
-        </div>
+  },[]);
+
+  const SearchHandler = () => {
+  setData(data => {
+  return { ...data, color: "blue" } 
+  })}
+
+
+  return (
+    <div>
+      <>{console.log({ data })}</>
+
+      <div className="header">
+        <img
+          className="header-img"
+          src="/logo-no-background.png"
+          width={200}
+          height={40}
+        />
+        <input
+          className="top-button"
+          type="search"
+          placeholder="Job Title / Keyword..."
+          onClick={SearchHandler}
+        />
+
+        <label htmlFor="location"></label>
+
+        <select id="location" name="location">
+          <option value="All">Wordwide</option>
+          <option value="Johannesburg">Johannesburg</option>
+          <option value="Cape Town">Cape Town</option>
+          <option value="Bloemfontein">Bloemfontein</option>
+          <option value="Pretoria">Pretoria</option>
+          <option value="Sandton">Sandton</option>
+          <option value="Durban">Durban</option>
+          <option value="East London">East London</option>
+        </select>
+
+        <button className="search-button">Search</button>
+
+        <img src="/login.png" className="header-image" width={60} height={50} />
       </div>
-    );
-  };
 
-export default HomePage
+      <div className="job-section">
+        <div className="job-text">
+          <h2>Find Your Perfect...</h2>
+        </div>
+        <img src="/search.png" alt="Search Image" className="job-image" />
+      </div>
 
+      <div></div>
+    </div>
+  );
+};
 
+export default HomePage;
